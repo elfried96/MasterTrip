b72e69fb4734887d993e2bfb65770f5a79e13c3a
KAlexRussel <kouawoualex1234@gmail.com>
2024-12-10 18:28:32 +0100

    update

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -2,7 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SplashScreen from '@/views/SplashScreen.vue'
 import BookingHotel from '@/views/BookingHotel.vue'
-import HotelView from '@/views/HotelView.vue'
+import HotelView from '@/views/Hotel/HotelView.vue'
+import DetailHotel from '@/views/Hotel/DetailHotel.vue'
+import RoomsDetails from '@/views/Rooms/RoomsDetails.vue'
+import RoomsView from '@/views/Rooms/RoomsView.vue'
+import OnBoarding from '@/views/OnBoarding/OnBoarding.vue'
+import OnBoardingTwo from '@/views/OnBoarding/OnBoardingTwo.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -23,15 +28,39 @@ const router = createRouter({
       component: HotelView,
     },
     {
-      path: '/booking',
-      name: 'booking',
+      path: '/bookings',
+      name: 'bookings',
       component: BookingHotel,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: '/details/:id',
+      name: 'details',
+      component: DetailHotel,
     },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: RoomsView,
+    },
+
+    {
+      path: '/detailsrooms/:id',
+      name: 'detailrooms',
+      component: RoomsDetails,
+    },
+    {
+      path: '/onboardingone',
+      name: 'onb',
+      component: OnBoarding,
+    },
+    {
+      path: '/onboard2',
+      name: 'onb2',
+      component: OnBoardingTwo,
+    },
+
+
+
   ],
 })
 
